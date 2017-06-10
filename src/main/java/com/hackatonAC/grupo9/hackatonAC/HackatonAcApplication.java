822c04c5532e8ef0249a4e5ff2a9bc82539a23e3
Pauloamorim <paulovitor.10@hotmail.com>
2017-06-10 10:17:20 -0300

    commit inicial

---
+++ src/main/java/com/hackatonAC/grupo9/hackatonAC/HackatonAcApplication.java
@@ -0,0 +1,12 @@
+package com.hackatonAC.grupo9.hackatonAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonAcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonAcApplication.class, args);
+	}
+}
