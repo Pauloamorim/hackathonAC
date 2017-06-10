9513db025cef6bfd13c2f4a29ee5ea9637d48b7a
Lorrany Ribeiro <lorrany.ribeiro360@gmail.com>
2017-06-10 17:41:18 -0300

    servico listar usuarios

--- src/main/java/com/hackatonAC/grupo9/hackatonAC/resources/UsuarioResources.java
+++ src/main/java/com/hackatonAC/grupo9/hackatonAC/resources/UsuarioResources.java
@@ -1,5 +1,7 @@
 package com.hackatonAC.grupo9.hackatonAC.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackatonAC.grupo9.hackatonAC.model.Localidade;
 import com.hackatonAC.grupo9.hackatonAC.model.Usuario;
 import com.hackatonAC.grupo9.hackatonAC.service.UsuarioService;
 
@@ -29,5 +32,13 @@ public class UsuarioResources {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Usuario>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioService.listar());
+
+	}
+
+	
 
 }
--- src/main/java/com/hackatonAC/grupo9/hackatonAC/service/UsuarioService.java
+++ src/main/java/com/hackatonAC/grupo9/hackatonAC/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.hackatonAC.grupo9.hackatonAC.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,11 @@ public class UsuarioService {
 	public Usuario obterUsuarioLogin(String login,String senha){
 
 		return usuarioRepository.findByloginAndSenha(login, senha);
+	}
+	
+	
+	public List<Usuario> listar(){
+		return usuarioRepository.findAll();
 	}
 
 }
